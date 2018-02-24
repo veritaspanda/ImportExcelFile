c7831850481918a9b417d3f788abbca219fc976f
Christopolis Tiberius <github@johnveritas.33mail.com>
2018-02-24 15:50:21 -0600

    corrected the import logic after installing the Microsoft ACE OLEDB driver for Access 2010 redistributable

--- ImportExcelFile/DataAccess.cs
+++ ImportExcelFile/DataAccess.cs
@@ -17,45 +17,25 @@ namespace ImportExcelFile
     {
         public static void importExcelFileData(DataGridView dataGridViewName, string fileName)
         {
-            String _tabName = "Data";
+            string _tabName = "Data";
             string _header = "YES";
-            String _conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 xml;HDR={1}'";
+            string _conStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 xml;HDR={1}'";
             _conStr = string.Format(_conStr, fileName, _header);
 
-            //OleDbConnection con = new OleDbConnection(_conStr);
-            //OleDbCommand oconn = new OleDbCommand("Select * From [" + _tabName + "$]", con);
-            //con.Open();
+            OleDbConnection con = new OleDbConnection(_conStr);
+            OleDbCommand oconn = new OleDbCommand("Select * From [" + _tabName + "$]", con);
+            con.Open();
 
 
-            //OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
-            //DataTable data = new DataTable();
-            //sda.Fill(data);
-            //dataGridViewName.DataSource = data;
+            OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dataGridViewName.DataSource = data;
 
 
-            //Read Data from the First Sheet.
-            using (OleDbConnection con = new OleDbConnection(_conStr))
-            {
-                using (OleDbCommand cmd = new OleDbCommand())
-                {
-                    using (OleDbDataAdapter oda = new OleDbDataAdapter())
-                    {
-                        DataTable dt = new DataTable();
-                        cmd.CommandText = "SELECT * From [" + _tabName + "]";
-                        cmd.Connection = con;
-                        con.Open();
-                        oda.SelectCommand = cmd;
-                        oda.Fill(dt);
-                        con.Close();
-
-                        //Populate DataGridView.
-                        dataGridViewName.DataSource = dt;
-                    }
-                }
-            }
         }
 
-
+        /*
         public static System.Data.DataTable GetExcelData(string location, string sheet, string tableName)
         {
             FileStream stream = File.Open(location, FileMode.Open, FileAccess.Read);
@@ -73,6 +53,6 @@ namespace ImportExcelFile
             excelReader.Close();
 
             return result.Tables[tableName];
-        }
+        } */
     }
 }
